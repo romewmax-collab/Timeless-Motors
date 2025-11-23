282fdf8d648673bdfe84c83f15406e0553c206e2
romewMx <romewmax@gmail.com>
2025-11-23 22:15:39 +0300

    Add files via upload

---
+++ post-pages/post-js.js
@@ -0,0 +1,15 @@
+const selectedCar = localStorage.getItem("selectedCar");
+
+function showTab() {
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => {
+    section.classList.remove("active");
+  });
+  if (selectedCar) {
+    const activeSection = document.getElementById(selectedCar);
+    if (activeSection) {
+      activeSection.classList.add("active");
+    }
+  }
+}
+document.addEventListener("DOMContentLoaded", showTab);
